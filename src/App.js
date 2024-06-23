deae28e54291ab22b7b4bc306f6117e3dbeecda9
Sabbir <sabbirkhan.kst178@gamil.com>
2024-06-23 16:55:11 +0530

    Changes the name and add some features

--- src/App.js
+++ src/App.js
@@ -1,81 +1,90 @@
+import { useState } from "react";
+import "./App.css";
+import About from "./component/About";
+import Navbar from "./component/Navbar";
+import TextForm from "./component/TextForm";
+import Alert from "./component/Alert";
 
-import { useState } from 'react';
-import './App.css';
-import About from './component/About';
-import Navbar from './component/Navbar';
-import TextForm from './component/TextForm';
-import Alert from './component/Alert';
-
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light'); //whether dark is enable or not
+  const [mode, setMode] = useState("light"); //whether dark is enable or not
   const [alert, setAlert] = useState(null);
 
   const showAlert = (massage, type) => {
     setAlert({
       msg: massage,
-      typ: type
+      typ: type,
     });
 
     setTimeout(() => {
       // delete setAlert();
-      setAlert(null)
+      setAlert(null);
     }, 2000);
-  }
+  };
 
-  const removeBodyClass=()=>{
-    document.body.classList.remove('bg-light')
-    document.body.classList.remove('bg-dark')
-    document.body.classList.remove('bg-warning')
-    document.body.classList.remove('bg-danger')
-    document.body.classList.remove('bg-success')
-  }
+  const removeBodyClass = () => {
+    document.body.classList.remove("bg-light");
+    document.body.classList.remove("bg-dark");
+    document.body.classList.remove("bg-warning");
+    document.body.classList.remove("bg-danger");
+    document.body.classList.remove("bg-success");
+  };
 
-  const  colours =(cls)=>{
-
+  const colours = (cls) => {
     removeBodyClass();
-    document.body.classList.add('bg-'+cls);
-  }
-  
+    document.body.classList.add("bg-" + cls);
+  };
 
   const toggleMode = () => {
     if (mode === "light") {
       setMode("dark");
       document.body.style.backgroundColor = "#042743";
-      showAlert("Dark mood has been enabled", "success")
-      document.title = ("dark mood is on")
+      showAlert("Dark mood has been enabled", "success");
+      document.title = "dark mood is on";
       // setInterval(() => {
       // document.title = ("dark mood in danger")
       // }, 2000);
       // setInterval(() => {
       // document.title = ("install dark mood now")
       // }, 1500);
-    } 
-    
-    else {
+    } else {
       setMode("light");
       document.body.style.backgroundColor = "white";
-      showAlert("Light mood has been enabled", "success")
-      document.title = ("light mood is on")
+      showAlert("Light mood has been enabled", "success");
+      document.title = "light mood is on";
     }
-  }
+  };
   return (
     <>
       <Router>
-        <Navbar title="TextUtlies" mode={mode} toggleMode={toggleMode} colors={colours}/>
+        <Navbar
+          title="TextFormatted"
+          mode={mode}
+          toggleMode={toggleMode}
+          colors={colours}
+        />
         <Alert alert={alert} />
         {/* <Navbar/> */}
-        
+
         <div className="container my-3">
           <Routes>
-            <Route exact path="/about" element = {<About mode={mode}/>}> </Route>
-            <Route exact path="/" element={ <TextForm showAlert={showAlert} heading="Enter The text To Analyze" mode={mode}/>}> </Route>
+            <Route exact path="/about" element={<About mode={mode} />}>
+              {" "}
+            </Route>
+            <Route
+              exact
+              path="/"
+              element={
+                <TextForm
+                  showAlert={showAlert}
+                  heading="Enter The text To Analyze Or Formatted"
+                  mode={mode}
+                />
+              }
+            >
+              {" "}
+            </Route>
           </Routes>
         </div>
       </Router>
